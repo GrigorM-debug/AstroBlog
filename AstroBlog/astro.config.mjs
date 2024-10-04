118626f0bb46e23e8def0c2d2046b51a1a243110
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-05 00:13:50 +0300

    Added Greeting Preact Component

--- AstroBlog/astro.config.mjs
+++ AstroBlog/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astro-blog-zeta-gules.vercel.app/"
+  site: "https://astro-blog-zeta-gules.vercel.app/",
+  integrations: [preact()]
 });
